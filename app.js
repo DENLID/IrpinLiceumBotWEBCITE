700ff8112812558f810877274d59c62aa4398bca
DENLID <127737950+DENLID@users.noreply.github.com>
2023-12-07 13:44:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.WebApp;
 tg.MainButton.setText("Відправити");
 tg.MainButton.show();
 
-WebApp.onEvent("mainButtonClicked", function(){
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
     let input1 = document.getElementById("input1").value;
     let input2 = document.getElementById("input2").value;
 
