a9885644fc16959270c9245148a25ddcae2cb8bf
DENLID <127737950+DENLID@users.noreply.github.com>
2023-12-07 16:20:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
     let class_letter = document.getElementById("class_letter").value;
     let ms_number = document.getElementById("ms_number").value;
     let students_number = document.getElementById("students_number").value;
+    let ms = document.getElementById("ms").value;
 
     data = {
         class_number: class_number,
         class_letter: class_letter,
         ms_number: ms_number,
-        students_number: students_number
+        students_number: students_number,
+        ms: ms
     }
     
     tg.sendData(JSON.stringify(data));
