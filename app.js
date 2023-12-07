1203dc425db846b0485355fb6a6e7a58a501071c
DENLID <127737950+DENLID@users.noreply.github.com>
2023-12-07 11:32:12 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let tg = window.Telegram.WebApp;
+let button = document.getElementById("b1");
+tg.MainButton.setText("Відправити");
+tg.MainButton.show();
+
+button.addEventListener("click", () => {
+    let input1 = document.getElementById("input1").value;
+    let input2 = document.getElementById("input2").value;
+
+    data = {
+        ms: input1,
+        class: input2
+    }
+    
+    tg.sendData(JSON.stringify(data));
+    tg.close();
+});
