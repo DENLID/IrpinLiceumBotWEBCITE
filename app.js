f8d1970cdd14b98f56d816e58f2e9213cb65a061
DENLID <127737950+DENLID@users.noreply.github.com>
2023-12-10 00:16:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let tg = window.Telegram.WebApp;
 tg.expand()
 tg.MainButton.setText("Відправити");
 tg.MainButton.show();
+tg.MainButton.disable()
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
     let class_number = document.getElementById("class_number").value;
@@ -21,3 +22,24 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
     tg.sendData(JSON.stringify(data));
     tg.close();
 });
+
+let ms_number = document.getElementById("ms_number");
+let students_number = document.getElementById("students_number");
+let ms = document.getElementById("ms");
+
+
+function updateSubmitBtn(){
+    const ms_numberv = ms_number.value.trim();
+    const msv = ms.value.trim();
+    const students_numberv = students_number.value.trim();
+    debugger;
+    if(ms_numberv && msv && students_numberv){
+        tg.MainButton.enable()
+    }else {
+        tg.MainButton.disable()
+    }
+}
+
+students_number.addEventListener('change', updateSubmitBtn);
+ms_number.addEventListener('change', updateSubmitBtn);
+ms.addEventListener('change', updateSubmitBtn);
